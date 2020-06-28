d8d351300e27d5c4edbfc2714fb0bb3dd14fb355
Mwaya Bolokonya <mwayab@gmail.com>
2020-06-28 21:34:02 +0200

    https://github.com/Chikwama-io/ChikwamaWalletContracts.git

--- contracts/CashPoints.sol
+++ contracts/CashPoints.sol
@@ -1,30 +1,22 @@
-pragma solidity >=0.5.0 <0.6.0;
+pragma solidity >=0.5.0 <0.7.0;
 
 contract CashPoints {
 uint public Count;
 
-struct Coordinate {
-    int _degrees;
-    uint _minutes;
-    uint _seconds;
-}
-
-
 struct CashPoint {
     string _name; //short Name
-    Coordinate _latitude;
-    Coordinate _longitude;
+    int _latitude;
+    int _longitude;
     uint _phoneNumber;
     uint rate; //local currency to usd rate
     uint endtime; //when time as cashpoint will expire
-    bool isCashPoint;
-    
+    bool isCashPoint;    
 }
 
 mapping (address=>CashPoint) cashpoints;
-event CreatedCashPoint(address cashpoint, string name, int latdeg, uint latmin, uint latsec,int longdeg, uint longmin, uint longsec, uint rate, uint endtime);
+event CreatedCashPoint(address cashpoint, string name, int lat, int long, uint rate, uint endtime);
 
-function addCashPoint(string memory name, int latdeg, uint latmin, uint latsec, int longdeg, uint longmin, uint longsec, uint phone, uint rate, uint duration) public{
+function addCashPoint(string memory name, int mylat, int mylong, uint phone, uint rate, uint duration) public{
    
     if(block.timestamp >= cashpoints[msg.sender].endtime)
     {
@@ -33,14 +25,13 @@ function addCashPoint(string memory name, int latdeg, uint latmin, uint latsec, 
     }
     
     require(!cashpoints[msg.sender].isCashPoint);
-    Coordinate memory myLat = Coordinate(latdeg, latmin, latsec);
-    Coordinate memory myLong = Coordinate(longdeg, longmin, longsec);
+  
     uint endtime = block.timestamp + duration * 1 days;
     bool isCashPoint = true;
 
-    cashpoints[msg.sender] = CashPoint(name, myLat, myLong, phone, rate, endtime, isCashPoint);
+    cashpoints[msg.sender] = CashPoint(name, mylat, mylong, phone, rate, endtime, isCashPoint);
     Count++;
-    emit CreatedCashPoint(msg.sender, name, latdeg, latmin, latsec, longdeg, longmin, longsec, rate, endtime);
+    emit CreatedCashPoint(msg.sender, name, mylat, mylong, rate, endtime);
 }
 
 
