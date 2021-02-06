92342ab5e56d0e1697275d0f1565af87da4ada59
Mwaya Bolokonya <mwayab@gmail.com>
2021-02-07 01:56:10 +0200

    Starting afresh

--- migrations/2_CashPoints.js
+++
@@ -1,6 +0,0 @@
-var cashpoints = artifacts.require("CashPoints");
-
- module.exports = function(deployer, network, accounts) {
-   // deployment steps
-   deployer.deploy(cashpoints, {from: accounts[0]});
- };
